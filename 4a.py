d465447a00be09993452e0fbb65f96e5a396ed3a
Stephen C. Moore <stephen.c.moore@gmail.com>
2021-12-08 10:44:59 -0600

    Added 4a

---
+++ 4a.py
@@ -0,0 +1,56 @@
+boards = {}
+board = {}
+entry = []
+itemcount = 0
+boardcount = 0
+
+with open("dataset4b.txt", "r") as items:
+    plays = items.readline().strip().split(',')
+    for item in items:
+        if item.strip():
+            for work in item.strip().split():
+                entry = [work, False]
+                board[itemcount] = entry
+                itemcount += 1
+                entry = []
+                if itemcount % 25 == 0:
+                    boards[boardcount] = board
+                    board = {}
+                    boardcount += 1
+                    itemcount = 0
+
+
+def mark_number(num):
+    for markboard in boards:
+        for position in boards[markboard]:
+            if boards[markboard][position][0] == num:
+                boards[markboard][position][1] = True
+
+
+def win_exists_in_board(check_board):
+    possible_wins = [list(range(5)), list(range(5, 10)), list(range(10, 15)), list(range(15, 20)), list(range(20, 25)),
+                     list(range(0, 21, 5)), list(range(1, 22, 5)), list(range(2, 23, 5)), list(range(3, 24, 5)),
+                     list(range(4, 25, 5))]
+    for win_type in possible_wins:
+        row_count = 0
+        for check_position in win_type:
+            if boards[check_board][check_position][1]:
+                row_count += 1
+        if row_count == 5:
+            return True
+
+
+def tally_board(check_board):
+    tally = 0
+    for position in boards[check_board]:
+        if not boards[check_board][position][1]:
+            tally = tally + int(boards[check_board][position][0])
+    return tally
+
+
+for play in plays:
+    mark_number(play)
+    for board in boards:
+        if win_exists_in_board(board):
+            print(f"Magic number: {tally_board(board) * int(play)}")
+            exit()
